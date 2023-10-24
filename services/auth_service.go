854d750163b0a8badbdc5359ed12b795104b65d8
Mizuha-hk <hk.haru0803@gmial.com>
2023-10-24 23:25:49 +0900

    [add] #1 add generating jwt function

---
+++ services/auth_service.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"onden-backend/config"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtConfig *config.JWTConfig;
+
+func AuthInit(config *config.JWTConfig) {
+	jwtConfig = config;
+}
+
+func GenerateJWTToken(userId string) (string, error){
+	secret_key := jwtConfig.SecretKey;
+	expirationTime := time.Now().Add(5 * time.Minute);
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userId,
+		"exp": expirationTime.Unix(),
+	});
+
+	return token.SignedString([]byte(secret_key));
+}
