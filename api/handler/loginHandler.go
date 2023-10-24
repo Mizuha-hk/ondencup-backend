6452a9c00edf6af9540d7584914127b2d62b6135
Mizuha-hk <hk.haru0803@gmial.com>
2023-10-24 23:24:27 +0900

    [modify] #1 to use auth service

--- api/handler/loginHandler.go
+++ api/handler/loginHandler.go
@@ -3,9 +3,8 @@ package handler
 import (
 	"net/http"
 	"onden-backend/api/models"
-	"time"
+	"onden-backend/services"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo/v4"
 )
 
@@ -20,24 +19,10 @@ func Login (c echo.Context) error {
 		return c.JSON(http.StatusUnauthorized, map[string]string{"message":"Invalid Credentials"});
 	}
 
-	token, err := generateJWTToken(user.ID);
+	token, err := services.GenerateJWTToken(user.ID);
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message":"Could not generate Token"});
 	}
 
 	return c.JSON(http.StatusOK, map[string]string{"token":token});
-}
-
-func generateJWTToken(userID string) (string, error) {
-	// シークレットキーを設定
-	secretKey := "your_secret_key" // 実際のアプリケーションでは、安全な方法でキーを管理してください
-	// トークンの有効期限を設定
-	expirationTime := time.Now().Add(24 * time.Hour)
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": userID,
-		"exp":     expirationTime.Unix(),
-	})
-
-	return token.SignedString([]byte(secretKey))
 }
