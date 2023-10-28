de0dbcdf173656facfa01fd72774c497ed5016f4
Mizuha-hk <hk.haru0803@gmial.com>
2023-10-28 20:10:58 +0900

    [modify] #5 add make finished handler

--- api/handler/roomHandler.go
+++ api/handler/roomHandler.go
@@ -31,7 +31,7 @@ func GetRooms(c echo.Context) error {
 
 	var rooms []models.Room;
 
-	db.DB.Limit(20).Offset(offset).Where("is_finished = ?", "FALSE").Find(&rooms);
+	db.DB.Limit(20).Offset(offset).Where("is_finished = ?", false).Find(&rooms);
 
 	return c.JSON(http.StatusOK, rooms);
 }
@@ -55,6 +55,36 @@ func GetRoomById(c echo.Context) error {
 	var room models.Room;
 
 	if err := db.DB.Where("id = ?", id).First(&room).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err);
+	}
+
+	return c.JSON(http.StatusOK, room);
+}
+
+func MakeFinished(c echo.Context) error {
+	user := c.Get("user")
+	if user == nil {
+		return c.JSON(http.StatusUnauthorized, "token is missing")
+	}
+	token := user.(*jwt.Token)
+	claims := token.Claims.(jwt.MapClaims);
+	userId := claims["user_id"].(string);
+	if userId == "" {
+		return c.JSON(http.StatusUnauthorized, "invalid user")
+	}
+
+	id, err := strconv.Atoi(c.Param("id"));
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err);
+	}
+
+	var room models.Room;
+	if err := db.DB.Where("id = ?", id).First(&room).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err);
+	}
+
+	room.IsFinished = true;
+	if err := db.DB.Save(&room).Error; err != nil {
 		return c.JSON(http.StatusInternalServerError, err);
 	}
 
