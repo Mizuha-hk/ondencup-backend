e2a032e5a21e9f0e4adf06df6683183248641a44
Mizuha-hk <hk.haru0803@gmial.com>
2023-10-28 19:39:57 +0900

    [modify] #3 get rooms with offset

--- api/handler/roomHandler.go
+++ api/handler/roomHandler.go
@@ -2,20 +2,37 @@ package handler
 
 import (
 	"net/http"
+	"onden-backend/api/models"
+	"onden-backend/db"
+	"strconv"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 )
 
-func GetAllRooms(c echo.Context) error {
+func GetRooms(c echo.Context) error {
+
 	user := c.Get("user")
 	if user == nil {
 		return c.JSON(http.StatusUnauthorized, "token is missing")
 	}
-
+		
 	token := user.(*jwt.Token)
-	claims := token.Claims.(jwt.MapClaims)
-	userId := claims["user_id"].(string)
+		
+	claims := token.Claims.(jwt.MapClaims);
+	userId := claims["user_id"].(string);
+	if userId == "" {
+		return c.JSON(http.StatusUnauthorized, "invalid user")
+	}
 
-	return c.JSON(http.StatusOK, map[string]string{"userId": userId})
+	offset, err := strconv.Atoi(c.QueryParam("offset"));
+	if err != nil {
+		offset = 0;
+	}
+
+	var rooms []models.Room;
+
+	db.DB.Limit(20).Offset(offset).Where("is_finished = ?", "FALSE").Find(&rooms);
+
+	return c.JSON(http.StatusOK, rooms);
 }
